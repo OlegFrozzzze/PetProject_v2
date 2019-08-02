dffb3c9135e084838af80440d18b5fb5e1639a96
Oleg Strelnikov <ji900@yandex.ru>
2019-08-03 00:49:42 +0300

    Changed project

--- src/main/java/com/example/springwithjpa/SpringwithjpaApplication.java
+++ src/main/java/com/example/springwithjpa/SpringwithjpaApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringwithjpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringwithjpaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwithjpaApplication.class, args);
+    }
 }
