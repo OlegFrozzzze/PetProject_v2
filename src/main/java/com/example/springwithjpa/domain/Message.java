399e05d05d92052c13f399eeff09ce194c77b7cb
Oleg Strelnikov <ji900@yandex.ru>
2019-08-18 23:25:18 +0300

    Add method (User author) and getter and setter

--- src/main/java/com/example/springwithjpa/domain/Message.java
+++ src/main/java/com/example/springwithjpa/domain/Message.java
@@ -14,8 +14,19 @@ public class Message {
     private String text;
     private String tag;
 
+    private User author;
+
     public Message() {
     }
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+
 
     public Message(String text, String tag) {
         this.text = text;
